006e938e86d300f1c2bf98c3cea0c00c67936ba0
Ykabrit <ykabrit@yanibou.fr>
2024-07-10 21:40:31 +0200

    Add information about all classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './app.css'
+import { Stack } from './components/Stack'
 
 const App = () => {
-  return null
+  return (
+    <Stack gap={2} style={{ alignItems: 'center' }}>
+      <div style={{ fontSize: '60px' }}>МРК БРГ ГЕН</div>
+    </Stack>
+  )
 }
 
 export default App
