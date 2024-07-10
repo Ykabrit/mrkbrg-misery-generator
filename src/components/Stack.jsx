bf071d4ca5ff13672ab3b5ef2484ebc9390a39ce
Ykabrit <ykabrit@yanibou.fr>
2024-07-10 17:35:58 +0200

    Add JSON rolltables and some preliminary work on the code

---
+++ src/components/Stack.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Stack = styled.div`
+  display: flex;
+  flex-direction: ${(props) =>
+    props.direction === 'horizontal' ? 'row' : 'column'};
+  gap: ${(props) => props.gap || 0}rem;
+`
