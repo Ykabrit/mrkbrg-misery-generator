b9fc0389c3853305400e36c87c4a4a5a06e1307a
Ykabrit <ykabrit@yanibou.fr>
2024-07-11 03:04:43 +0200

    Add classless generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './app.css'
-import { Stack } from './components/Stack'
+import { useState } from 'react';
+import './app.css';
+import { Stack } from './components/Stack';
+import Character from './character';
 
 const App = () => {
+  const [classes, setClasses] = useState([
+    'deserter',
+    'herbmaster',
+    'heretic',
+    'hermit',
+    'royalty',
+    'scum',
+  ]);
+
+  const char = new Character([]);
+  console.log(char.backstory.personality);
+
   return (
-    <Stack gap={2} style={{ alignItems: 'center' }}>
+    <Stack $gap={2} style={{ alignItems: 'center' }}>
       <div style={{ fontSize: '60px' }}>МРК БРГ ГЕН</div>
     </Stack>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,8 +1,8 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Stack = styled.div`
   display: flex;
   flex-direction: ${(props) =>
     props.direction === 'horizontal' ? 'row' : 'column'};
-  gap: ${(props) => props.gap || 0}rem;
-`
+  gap: ${(props) => props.$gap || 0}rem;
+`;
