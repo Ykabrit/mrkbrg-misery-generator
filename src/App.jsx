bf592b2490b37cac0fbcae469c65f730f9068546
Ykabrit <ykabrit@yanibou.fr>
2024-08-11 16:17:01 +0200

    Add a footer with the MB copyright notice.

--- src/App.jsx
+++ src/App.jsx
@@ -92,6 +92,14 @@ const App = () => {
             </SkillsContainer>
             <Equipment equipment={character.equipment} />
           </div>
+          <footer className="mt-5 min-h-10 w-screen border-t-2 border-t-white pt-5 text-center font-mono text-sm">
+            <span className="font-bold text-mbyellow">ГЕНЕРАТОР УБОЖЕСТВ</span>{' '}
+            is an independent production by{' '}
+            <span className="font-bold text-mbyellow">Yanibou</span> and is not
+            affiliated with Ockult Örtmästare Games or Stockholm Kartell. It is
+            published under the MÖRK BORG Third Party License. MÖRK BORG is
+            copyright Ockult Örtmästare Games and Stockholm Kartell.
+          </footer>
         </>
       )}
     </div>
