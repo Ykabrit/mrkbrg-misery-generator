95c3600c0ef386bb646a669b24a9a7df867e01d5
Ykabrit <ykabrit@yanibou.fr>
2024-08-11 15:10:44 +0200

    Change some text in Skills.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,7 +14,7 @@ const Skills = ({ skills, decoctions }) => {
       ))}
       {decoctions && (
         <div className="flex flex-col items-center gap-1">
-          <div className="font-bold text-mbpink">Отвары (2d8)</div>
+          <div className="font-bold text-mbpink">Отвары (2d8 в день)</div>
           <ol className="list-decimal pl-7 lg:pl-0">
             {Object.keys(decoctions).map((key) => (
               <li key={`${key}-decoction`}>
