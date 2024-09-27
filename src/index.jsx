7a98acc8447fdc7a2e6398181d04fd51e997ec6a
Ykabrit <ykabrit@yanibou.fr>
2024-09-27 14:51:21 +0200

    Remove StrictMode.

--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,7 @@ import { CharacterProvider } from './context/CharacterProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <CharacterProvider>
       <App />
-    </CharacterProvider>
-  </React.StrictMode>,
+    </CharacterProvider>,
 );
